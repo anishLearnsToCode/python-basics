5104c8dd24c9bebf16d7d92dbc023279751e4150
anishSachdeva <anish_@outlook.com>
2018-05-21 19:49:08 +0530

    completed strings.py file

--- 2-Strings.py
+++ 2-Strings.py
@@ -1,2 +1,10 @@
 print('This is a sample string with \n newline character')
 print(r'This is a sample string with \n newline character which has been ignored')
+
+firstName = 'Anish'
+lastName = 'Sachdeva'
+fullName = firstName + ' ' + lastName
+print(fullName)
+
+firstNameMultiplied = firstName * 5
+print(firstNameMultiplied)
